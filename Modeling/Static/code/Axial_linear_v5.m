e8d5d498c6833ec0b4850760a950780993a0affd
mohsen <mogolzardian@gmail.com>
2025-05-07 01:20:59 -0400

    Update Axial_linear_v5.m

--- Modeling/Static/code/Axial_linear_v5.m
+++ Modeling/Static/code/Axial_linear_v5.m
@@ -72,14 +72,14 @@ F = zeros(nDOF,1);
 % Moment 1
 force_node_1      = 26;
 force_DOF_local_1 = 3;   % rotation (theta)
-moment_value_1    = 800; % Nm
+moment_value_1    = -800; % Nm
 globalDOF_1 = (force_node_1-1)*nDOF_per_node + force_DOF_local_1;
 F(globalDOF_1) = moment_value_1;
 
 %  Moment 2 
 force_node_2      = 35;
 force_DOF_local_2 = 3;    % rotation (theta)
-moment_value_2    = -800; % Nm in opposite direction
+moment_value_2    = +800; % Nm in opposite direction
 globalDOF_2 = (force_node_2-1)*nDOF_per_node + force_DOF_local_2;
 F(globalDOF_2) = moment_value_2;
 
@@ -102,9 +102,9 @@ vertical_disp = U(2:nDOF_per_node:end);
 figure;
 plot(x_nodes, vertical_disp, 'b-o','LineWidth',2,'MarkerFaceColor','r');
 grid on;
-xlabel('Beam length (m)');
-ylabel('Vertical deflection (m)');
-title('Static Deflection of Beam with Axial DOF');
+xlabel('beam length (m)');
+ylabel('vertical deflection (m)');
+title('static deflection of beam with axial DOF');
 
 %% Modal analysis
 % Solve generalized eigenvalue problem: K_reduced * phi = omega^2 * M_reduced * phi
@@ -116,7 +116,7 @@ freq = sqrt(omega2)/(2*pi);
 
 % Display first few natural frequencies
 nModesToShow = 5;
-disp('First natural frequencies (Hz):');
+disp('natural frequencies (Hz):');
 disp(freq(1:nModesToShow));
 
 % Plot first nModesToShow mode shapes (vertical deflection)
@@ -127,31 +127,31 @@ for j = 1:nModesToShow
     vmode = mode_full(2:nDOF_per_node:end);
     subplot(nModesToShow,1,j);
     plot(x_nodes, vmode, '-o','LineWidth',1.5);
-    ylabel(sprintf('Mode %d', j));
+    ylabel(sprintf('mode %d', j));
     grid on;
     if j==1
-        title('First Vertical Mode Shapes');
+        title('mode shapes');
     end
     if j==nModesToShow
-        xlabel('Beam length (m)');
+        xlabel('beam length (m)');
     end
 end
 
-%% Rayleigh damping
-% Target damping ratios for first two modes
-zeta1 = 0.02;  % 2% in mode 1
-zeta2 = 0.02;  % 2% in mode 2
+%% Rayleigh damping calculation
+%     damping ratios for first two modes
+zeta1 = 0.02;  % 2% for mode 1
+zeta2 = 0.02;  % 2% for mode 2
 omega1 = 2*pi*freq(1);
 omega2 = 2*pi*freq(2);
 
-% Solve for alpha and beta: zeta_i = alpha/(2*omega_i) + beta*omega_i/2
+% deriving the alpha and beta: zeta_i = alpha/(2*omega_i) + beta*omega_i/2
 Acoef = [1/(2*omega1), omega1/2; 1/(2*omega2), omega2/2];
 bcoef = [zeta1; zeta2];
 ab = Acoef \ bcoef;
 alpha = ab(1);
 beta  = ab(2);
 
-% Assemble damping matrix
+% Assembled damping matrix
 C_reduced = alpha * M_reduced + beta * K_reduced;
 
 disp('Rayleigh damping coefficients:');
@@ -160,3 +160,87 @@ fprintf('  alpha = %.3e  beta = %.3e\n', alpha, beta);
 % (Optional) Dynamic stiffness at a given frequency
 % omega_test = 2*pi*10;  % 10 Hz
 % Kdyn = -omega_test^2 * M_reduced + 1i*omega_test * C_reduced + K_reduced;
+
+
+%% Newmark-Beta Time Integration (Impulse Response)
+
+% parameters for time integration
+beta   = 1/4;
+gamma  = 1/2;
+dt     = 1e-4;       
+Tmax   = 0.1;        
+Nt     = round(Tmax / dt);
+time   = (0:Nt)*dt;
+
+% initial conditions
+w0  = zeros(length(freeDOF),1);  % initial displacement
+v0  = zeros(length(freeDOF),1);  % initial velocity
+a0  = M_reduced \ (F_reduced - C_reduced*v0 - K_reduced*w0);  % initial acceleration
+
+% history arrays
+W  = zeros(length(w0), Nt+1);  W(:,1) = w0;
+V  = zeros(length(w0), Nt+1);  V(:,1) = v0;
+A  = zeros(length(w0), Nt+1);  A(:,1) = a0;
+
+% newmark coefficients
+a0N = 1 / (beta*dt^2);
+a1N = gamma / (beta*dt);
+a2N = 1 / (beta*dt);
+a3N = 1 / (2*beta) - 1;
+a4N = gamma / beta - 1;
+a5N = dt * (gamma / (2*beta) - 1);
+
+% Effective stiffness matrix
+Keff = K_reduced + a1N * C_reduced + a0N * M_reduced;
+
+% impulse force imposing 
+f_dyn = zeros(length(w0), Nt+1);
+impulse_node = round(nElem/2) + 1;
+impulse_DOF  = 2; % vertical DOF (w)
+impulse_val  = 1000;   % N
+impulse_time = 0.001;  % 1 ms duration
+impulse_idx  = (impulse_node-1)*nDOF_per_node + impulse_DOF;
+impulse_dof  = find(freeDOF == impulse_idx);  % local DOF in reduced system
+impulse_steps = round(impulse_time / dt);
+f_dyn(impulse_dof, 1:impulse_steps) = impulse_val;
+
+% time integration main loop
+for n = 1:Nt
+    % effective force
+    Feff = f_dyn(:,n+1) ...
+         + M_reduced * (a0N*W(:,n) + a2N*V(:,n) + a3N*A(:,n)) ...
+         + C_reduced * (a1N*W(:,n) + a4N*V(:,n) + a5N*A(:,n));
+
+    % solve for displacement
+    W(:,n+1) = Keff \ Feff;
+
+    % acceleration and velocity updates
+    A(:,n+1) = a0N * (W(:,n+1) - W(:,n)) - a2N * V(:,n) - a3N * A(:,n);
+    V(:,n+1) = V(:,n) + dt * ((1 - gamma) * A(:,n) + gamma * A(:,n+1));
+end
+
+% plot dynamic response at midspan
+mid_dof = find(freeDOF == impulse_idx);
+figure;
+plot(time, W(mid_dof,:), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Vertical displacement at midpoint (m)');
+title('Dynamic response to impulse force (Newmark-Beta)');
+grid on;
+
+% plot velocity at midpoint
+figure;
+plot(time, V(mid_dof,:), 'r', 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Velocity at midpoint (m/s)');
+title('Velocity response at midpoint (Newmark-Beta)');
+grid on;
+
+% plot acceleration at midpoint
+figure;
+plot(time, A(mid_dof,:), 'g', 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Acceleration at midpoint (m/s^2)');
+title('Acceleration response at midpoint (Newmark-Beta)');
+grid on;
+
